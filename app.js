a48fce5b6bf4c72c97f83b19889f535bfcbe7f73
Mohammed Mishal <mishal2mk2@gmail.com>
2022-11-11 00:26:26 +0530

    server.js set and used import

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-const fs = require('fs')
-const express = require('express');
-const logger = require('morgan');
+import express from "express";
+import logger from "morgan";
 
 const app = express();
 
 
 app.use(logger('dev'));
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(`./public`));
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'Hello from the server side!!', app: 'ZAIN' });
 })
 
 
-const PORT = 3000;
-app.listen(PORT, (error) => {
-    if (!error) {
-        console.log("Server is successfully Running, and App is listening to port " + PORT);
-    } else {
-        console.log("Error occurred, server can't start", error);
-    }
-})
+export default app;
---
+++ server.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({ path: "./config.env" })
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log("Server is successfully Running, and App is listening to port " + PORT);
+    } else {
+        console.log("Error occurred, server can't start", error);
+    }
+})
