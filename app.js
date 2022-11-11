70490ed80dd46e89a2622507b27594eedcfc9e25
Mohammed Mishal <mishal2mk2@gmail.com>
2022-11-11 06:51:04 +0530

    user home page created

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('user/layout');
+  res.render('user/home');
 });
 
 export default app;
