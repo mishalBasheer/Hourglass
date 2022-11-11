9c96e544dffc7588393a3159bc655e57d2ccf2da
Mohammed Mishal <mishal2mk2@gmail.com>
2022-11-11 07:01:53 +0530

    category shop added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('user/home');
+  res.render('user/shop');
 });
 
 export default app;
