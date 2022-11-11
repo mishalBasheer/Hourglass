9d2fd8813d7629f88bf2292f3e8ce56045cc0e6b
Mohammed Mishal <mishal2mk2@gmail.com>
2022-11-11 06:33:07 +0530

    user and admin partials set and u ejs file created

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-import express from "express";
-import logger from "morgan";
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.static(`./public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.status(200).send({ message: 'Hello from the server side!!', app: 'ZAIN' });
-})
-
+  res.render('user/layout');
+});
 
 export default app;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-import dotenv from "dotenv";
-import app from "./app.js";
+import dotenv from 'dotenv';
 
-dotenv.config({ path: "./config.env" })
+import app from './app.js';
+
+dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
-    if (!error) {
-        console.log("Server is successfully Running, and App is listening to port " + PORT);
-    } else {
-        console.log("Error occurred, server can't start", error);
-    }
-})
+  if (!error) {
+    console.log(`Server is successfully Running, and App is listening to port ${PORT}`);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
