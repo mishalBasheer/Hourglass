fe8773ee43845673b38e9177e3b410771f20cc0c
Mohammed Mishal <mishal2mk2@gmail.com>
2022-11-11 07:09:21 +0530

    login signup forgotpass done templating

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('user/shop');
+  res.render('user/forgotpass');
 });
 
 export default app;
