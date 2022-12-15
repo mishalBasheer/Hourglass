361aad1d36ec01dc4db91051cc110562194e3b45
Mohammed Mishal <mishal2mk2@gmail.com>
2022-12-15 21:09:26 +0530

    connected to clustter

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 const DBL = process.env.DATABASE_LOCAL;
 
-const mongoos = mongoose.connect(DBL, {
+const mongoos = mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
